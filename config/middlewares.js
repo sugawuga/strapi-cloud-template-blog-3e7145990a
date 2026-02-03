049989e073f259aeb59397380d83ec937562c4b4
sugawuga <sugamsht@gmail.com>
2026-02-03 23:41:49 +0545

    feat: update CORS middleware to use a whitelist for origins and specify allowed headers feat: enhance enquiry schema by adding missing attributes for title, firstName, lastName, and submittedAt

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,9 +5,29 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: '*',
-      headers: '*',
+      // Use a small whitelist of known frontends to avoid issues with credentials and preflight handling.
+      origin: (ctx) => {
+        const whitelist = [
+          'https://saurya-adventure-5189fedd6ed7.herokuapp.com',
+          'http://localhost:3000',
+          'http://localhost:5000',
+          'https://aistudio.google.com/',
+        ];
+        const requestOrigin = ctx.request.header.origin;
+        return whitelist.includes(requestOrigin) ? requestOrigin : false;
+      },
+      // Explicit list of allowed request headers
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      // If you later need cookies/auth credentials, set this to true and be sure the origin is not '*'
+      credentials: false,
+      maxAge: 86400,
     },
   },
   'strapi::poweredBy',
